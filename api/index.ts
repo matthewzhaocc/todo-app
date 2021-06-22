6c4cccb1ef1bbdc088ac8f7935d9a720a0c2b536
Zhixiang Zhao <matthew@mail.matthew-cloud.com>
2021-06-21 22:31:25 -0700

    rate limit + delete

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import AWS from 'aws-sdk';
 import {body, validationResult} from 'express-validator';
+import rateLimit from 'express-rate-limit';
 // environment settings
 const tableName = process.env.TABLE_NAME;
 
@@ -21,6 +22,10 @@ interface todoItem {
 
 api.use(morgan('common'));
 api.use(express.json());
+api.use('/api/', rateLimit({
+  max: 200,
+  windowMs: 15 * 60 * 100,
+}));
 
 api.post(
     '/api/todo',
@@ -31,7 +36,7 @@ api.post(
       if (!errors.isEmpty()) {
         return res.status(400).send('Invalid body');
       }
-      const statement = `INSERT INTO todo value {'name': ?,'description': ?};`;
+      const statement = `INSERT INTO todo VALUE {'name': ?,'description': ?};`;
       try {
         await DDB.executeStatement({
           Statement: statement,
@@ -67,6 +72,27 @@ api.get('/api/todo', async (req, res) => {
   }
   await res.json(todoItems);
 });
+
+api.delete('/api/todo',
+    body('name').isLength({min: 3}),
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).send('Invalid Request Body');
+      }
+      const statement = `DELETE FROM ${tableName} WHERE name = ?;`;
+      try {
+        await DDB.executeStatement({
+          Statement: statement,
+          Parameters: [
+            {'S': req.body.name},
+          ],
+        });
+        return res.status(200).send('success');
+      } catch {
+        return res.status(500).send('something went wrong');
+      }
+    });
 
 api.listen(process.env.PORT || 3000, () => {
   console.log(`application listening on ${process.env.PORT || 3000}`);
